65c785c34bc00e7f9e7e1560d125f2c89f90fb3f
Samuel Bosawer <zam.bosawer@gmail.com>
2025-06-11 10:15:41 +0900

    update route pdf

--- resources/views/admin/pengaduan/create.blade.php
+++ resources/views/admin/pengaduan/create.blade.php
@@ -57,7 +57,7 @@
                                     <div class="col-md-2 m-2">
                                         @if (Auth::user()->hasRole('kepaladinas|pendampingdinas|kepalabidang') && Request::segment(3) == 'detail')
 
-                                        <a class="btn btn-danger" target="_blank" href="{{ route('dashboard.pengaduan.pdf.detail',$data->id) }}">
+                                        <a class="btn btn-danger" target="_blank" href="{{ url('dashboard/pengaduan/pdfdetail/cetak/'.$data->id) }}">
                                             Cetak PDF <i data-feather="file-text"></i></a>
 
                                         @endif
